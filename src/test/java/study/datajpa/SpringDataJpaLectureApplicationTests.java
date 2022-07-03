6ca3b027419f31e27b4f08a10897616bf5dbcbed
minsoo.lee <letomok@gmail.com>
2022-07-03 19:21:21 +0900

    공통 인터페이스 분석

--- src/test/java/study/datajpa/SpringDataJpaLectureApplicationTests.java
+++ src/test/java/study/datajpa/SpringDataJpaLectureApplicationTests.java
@@ -2,8 +2,10 @@ package study.datajpa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootTest
+@EnableJpaRepositories(basePackages = "study.datajpa.repository")
 class SpringDataJpaLectureApplicationTests {
 
     @Test
