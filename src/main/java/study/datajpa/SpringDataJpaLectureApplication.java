bee6e655aacf95833d577b6dc5580e4490cdbcb8
minsoo.lee <letomok@gmail.com>
2022-07-03 16:20:17 +0900

    프로젝트 설정

---
+++ src/main/java/study/datajpa/SpringDataJpaLectureApplication.java
@@ -0,0 +1,13 @@
+package study.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaLectureApplication.class, args);
+    }
+
+}
