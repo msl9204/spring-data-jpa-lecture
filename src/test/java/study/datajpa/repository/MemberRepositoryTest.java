d00924aa685f1baf387a8241d592c611376d739b
minsoo.lee <letomok@gmail.com>
2022-07-05 00:27:49 +0900

    Jpa NamedQuery

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -85,4 +85,16 @@ class MemberRepositoryTest {
     void findTop3HelloBy() {
         List<Member> top3HelloBy = memberRepository.findTop3HelloBy();
     }
+
+    @Test
+    void testNamedQuery() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsername("AAA");
+        Member findMember = result.get(0);
+        assertThat(findMember).isEqualTo(m1);
+    }
 }
