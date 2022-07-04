3ed7e0eaeb5a5df654ebd91cc4885165f13e36ef
minsoo.lee <letomok@gmail.com>
2022-07-04 20:32:52 +0900

    메소드 이름으로 쿼리 생성

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -62,4 +62,27 @@ class MemberRepositoryTest {
         long deletedCount = memberRepository.count();
         assertThat(deletedCount).isEqualTo(0);
     }
+
+    @Test
+    void findByUsernameAndAgeGreaterThenTest() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    void findHelloBy() {
+        List<Member> helloBy = memberRepository.findHelloBy();
+    }
+
+    @Test
+    void findTop3HelloBy() {
+        List<Member> top3HelloBy = memberRepository.findTop3HelloBy();
+    }
 }
